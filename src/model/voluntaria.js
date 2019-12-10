371a2738958f2c2b08672c42897ce2376220e23e
daramedeiros <daraamedeiros@outlook.com>
2019-12-10 20:10:30 -0300

    criando model voluntaria

---
+++ src/model/voluntaria.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const VoluntariaSchema = new mongoose.Schema ({
+    nome: {type: String},
+    sobrenome: {type: String},
+    email:{type: String},
+    cepAtendimento: {type: Number},
+    telefoneAtendimento: {type: Number},
+    whatsapp: {type: String}
+}
+, {
+    versionKey: false
+})
+
+
+const Voluntaria = mongoose.model('Voluntaria', VoluntariaSchema);
