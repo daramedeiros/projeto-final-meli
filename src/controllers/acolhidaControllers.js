a0c71e6d39deca21158682d63ddb6cdd930e77b1
daramedeiros <daraamedeiros@outlook.com>
2019-12-10 20:18:56 -0300

    atualizando controller acolhida

--- src/controllers/acolhidaControllers.js
+++ src/controllers/acolhidaControllers.js
@@ -4,8 +4,6 @@ const Acolhida = require('../model/acolhida')
 const fs = require('fs');
 
 exports.get = (req, res) => {
-  // console.log(req.url)
-  // res.status(200).send(alunas)
   Acolhida.find(function (err, acolhida) {
     if (err) res.status(500).send(err)
     res.status(200).send(acolhida);
