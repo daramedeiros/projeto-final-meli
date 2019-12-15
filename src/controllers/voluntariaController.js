92c1345652977a73ca531b78776b6e557ab965cb
daramedeiros <daraamedeiros@outlook.com>
2019-12-15 12:45:17 -0200

    metodos da entidade voluntaria concluidos

--- src/controllers/voluntariaController.js
+++ src/controllers/voluntariaController.js
@@ -19,3 +19,47 @@ exports.post = (req, res) => {
 
   })
 }
+
+exports.getById = (req, res) => {
+  const voluntariaId = req.params.id
+
+  Voluntaria.findById(voluntariaId, function (err, voluntaria) {
+    if (err) return res.status(500).send(err);
+
+    if (!voluntaria) {
+      return res.status(200).send({ message: `Infelizmente não localizamos a voluntaria pelo id: ${voluntariaId}` });
+    }
+
+    res.status(200).send(voluntaria);
+  })
+}
+
+exports.update = (req, res) => {  
+  Voluntaria.update(
+    { _id: req.params.id },
+    { $set: req.body },
+    { upsert: true },
+    function (err) {
+      if (err) return res.status(500).send({ message: err });
+      res.status(200).send({ message: "Atualizado com sucesso!" });
+    })
+}
+
+
+exports.deletarVoluntaria = (req, res) => {
+  const idVoluntaria = req.params.id;
+
+  Voluntaria.findById(idVoluntaria, function (err, voluntaria) {
+    if (err) return res.status(500).send(err);
+
+    if (!voluntaria) {
+      return res.status(200).send({ message: `Infelizmente não localizamos a voluntaria pelo id: ${req.params.id}` });
+    }
+
+    voluntaria.remove(function (err) {
+      if (!err) {
+        res.status(200).send({ message: 'Voluntaria removida com sucesso...' });
+      }
+    })
+  })
+}
--- src/routes/voluntariaRoute.js
+++ src/routes/voluntariaRoute.js
@@ -5,5 +5,9 @@ const controller = require("../controllers/voluntariaController")
 router.get("/", controller.get)
 
 router.post("/", controller.post)
+router.get("/:id", controller.getById)
+
+router.put("/:id", controller.update)
+router.delete("/:id", controller.deletarVoluntaria)
 
 module.exports = router
