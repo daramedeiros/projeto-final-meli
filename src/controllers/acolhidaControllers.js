b71d75356b64efd540f05b11231328988ad031b8
daramedeiros <daraamedeiros@outlook.com>
2019-12-15 12:04:01 -0200

    criando os metodos da entidade acolhida

--- src/controllers/acolhidaControllers.js
+++ src/controllers/acolhidaControllers.js
@@ -21,6 +21,50 @@ exports.post = (req, res) => {
   })
   }
 
+  exports.getById = (req, res) => {
+    const acolhidaId = req.params.id
+  
+    Acolhida.findById(acolhidaId, function (err, acolhida) {
+      if (err) return res.status(500).send(err);
+  
+      if (!acolhida) {
+        return res.status(200).send({ message: `Infelizmente não localizamos a pessoa acolhida pelo id: ${acolhidaId}` });
+      }
+  
+      res.status(200).send(acolhida);
+    })
+  }
+  
+  exports.update = (req, res) => {  
+    Acolhida.update(
+      { _id: req.params.id },
+      { $set: req.body },
+      { upsert: true },
+      function (err) {
+        if (err) return res.status(500).send({ message: err });
+        res.status(200).send({ message: "Atualizado com sucesso!" });
+      })
+  }
+  
+  
+  exports.deletarPosto = (req, res) => {
+    const idAcolhida = req.params.id;
+  
+    Acolhida.findById(idAcolhida, function (err, acolhida) {
+      if (err) return res.status(500).send(err);
+  
+      if (!acolhida) {
+        return res.status(200).send({ message: `Infelizmente não localizamos a pessoa acolhida pelo id: ${req.params.id}` });
+      }
+  
+      acolhida.remove(function (err) {
+        if (!err) {
+          res.status(200).send({ message: 'Pessoa acolhida removida com sucesso...' });
+        }
+      })
+    })
+  }
+
 
 
 
--- src/routes/acolhidaRoute.js
+++ src/routes/acolhidaRoute.js
@@ -2,8 +2,14 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/acolhidaControllers")
 
+
 router.get("/", controller.get)
 
 
 router.post("/", controller.post)
+router.get("/:id", controller.getById)
+
+router.put("/:id", controller.update)
+router.delete("/:id", controller.deletarPosto)
+
 module.exports = router
