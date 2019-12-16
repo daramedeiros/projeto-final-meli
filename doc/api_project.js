6c3b59f47fa695bf0865dcc336624698341757a5
daramedeiros <daraamedeiros@outlook.com>
2019-12-16 15:11:40 -0200

    update APIdoc

--- doc/api_project.js
+++ doc/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-12-16T17:06:36.845Z",
+    "time": "2019-12-16T17:10:13.131Z",
     "url": "http://apidocjs.com",
     "version": "0.19.0"
   }
