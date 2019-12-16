3756febf7d9535e763d1475f28ce15838904dad8
daramedeiros <daraamedeiros@outlook.com>
2019-12-16 15:36:33 -0200

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./src/app")
-const port = process.env.port || 3000;
+const port = 3000 || process.env.PORT;
 
 app.listen(port, function() {
   console.log(`app está rodando na porta ${port}`)
